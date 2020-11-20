e97b49d5475469128fe568893419650d240fa29f
P.S Yan <1050335889@sjtu.edu.cn>
2020-11-20 21:35:21 +0800

    add M2006 mode

--- RM-frame-reconstruction-d54d3127e30bb398860f4885f6fd24f4243bf56b/Additional/Src/Additional.cpp
+++ RM-frame-reconstruction-d54d3127e30bb398860f4885f6fd24f4243bf56b/Additional/Src/Additional.cpp
@@ -14,15 +14,16 @@ void Additional::Reset() {
 	  initRecvUart();
 	  initVisionRecvUart();
     M2006.Reset(CAN_TYPE_1, 0x206, 36,
-                8.0, 0.2, 5, 1580.0, 1080.0, 1080.0, 2000.0,
+                8.2, 0.2, 5, 1580.0, 1080.0, 1080.0, 2000.0,
                 15.0, 0.01, 0.5, 1000.0, 5000.0, 10000.0, 10000.0);
 		//Reset(CanType_e can, uint16_t _RxID, double _reductionRate, double _kp = 8.0, double _ki = 0.2, double _kd = 5, double _pMax, double _iMax,
     //              double _dMax, double _max)
 }
 
 void Additional::setTargetAngle(int16_t Angle) {
-	  double range = 5;
-		double inc = abs(sin(2*M2006.targetAngle)*sin(2*M2006.targetAngle)) * range + 0.1;
+	/*
+	  double range = 10;
+		double inc = abs(sin(2*M2006.targetAngle)*sin(2*M2006.targetAngle)) * range + 0.5;
 		
 	  if (M2006.targetAngle>180) {
 			M2006.targetAngle = M2006.targetAngle-inc;
@@ -30,10 +31,31 @@ void Additional::setTargetAngle(int16_t Angle) {
 		else {
 			M2006.targetAngle = Angle;
 		}
+	*/
+	if (M2006.targetAngle>180) {
+		M2006.targetAngle = M2006.targetAngle-10;
+	}
+	else {
+		M2006.targetAngle = Angle;
+	}
 }
 
 
 void Additional::setRotate() {
+		//mode 0: directly add angle every time
+		if(M2006.targetAngle<1800)
+				M2006.targetAngle = M2006.targetAngle+10;
+		
+		
+		//mode 1: sine function
+		/*
+	  double range = 10;
+		double inc = abs(sin(2*M2006.targetAngle)*sin(2*M2006.targetAngle)) * range + 0.5;
+    M2006.targetAngle = M2006.targetAngle+inc;
+		*/
+	
+	/*
+		//mode 2: Clockwise 180, stay and Counterclockwise 180
 		if (times % 3 == 0) {
 				if(ang == 18){
 						if(lp<5){
@@ -56,12 +78,10 @@ void Additional::setRotate() {
 		ang %= 36;
 		int nowangle = 180 - 10 * abs(18-ang);
 		M2006.targetAngle = nowangle;
-    //M2006.targetAngle = M2006.targetAngle+10;
-		/*
-	  double range = 5;
-		double inc = abs(sin(2*M2006.targetAngle)*sin(2*M2006.targetAngle)) * range + 0.1;
-    M2006.targetAngle = M2006.targetAngle+inc;
 		*/
+		
+		//mode 3:
+		
 }
 
 Additional Additional::additional;
